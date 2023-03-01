e6beec4537aced47d11d4e38037f5982d6fa3e3a
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-02-28 21:25:38 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,7 +1,7 @@
 return {{[ [[Trades]] ] = {2},[ [[Basher]] ] = {3},[ [[Legacy GUI]] ] = {4},[ [[Limb Tracker]] ] = {5},[ [[Soundtrack]] ] = {6},},
 {[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Legacy%20Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
 {[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Newest Version as of 12.4.22, Now includes rage information for every class. This will need more information for Elemental Lords. If you installed a previous version you will want to do: lua Legacy.Settings.Basher.Classes = {}. You will have to reset your class attack, prealias and now non-rage shield attack. Sorry for the trouble.]],},
-{[ [[link]] ] = [[https://github.com/Legacy-Achaea/Legacy/raw/main/Legacy%20UI.mpackage]],[ [[desc]] ] = [[Heyo],},
+{[ [[link]] ] = [[https://github.com/Legacy-Achaea/Legacy/raw/main/Legacy%20UI.mpackage]],[ [[desc]] ] = [[Hey my guy thsis is a test]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
 {[ [[link]] ] = [[https://github.com/MegophrysNasuta/SOUNDTRACK/raw/main/SOUNDTRACK.mpackage]],[ [[desc]] ] = [[Nasuta's Ambient Soundtracks, changes based on environments]],},
 }
