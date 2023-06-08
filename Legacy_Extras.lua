ece8c930d025a788b5fcc85cc9c99fe8394d34b7
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-06-08 03:22:00 -0500

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,7 +1,8 @@
-return {{[ [[Trades]] ] = {2},[ [[Basher]] ] = {3},[ [[Legacy GUI]] ] = {4},[ [[Limb Tracker]] ] = {5},[ [[Soundtrack]] ] = {6},},
+return {{[ [[Trades]] ] = {2},[ [[Legacy GUI]] ] = {3},[ [[Basher]] ] = {4},[ [[Curing Swaps]] ] = {5},[ [[Limb Tracker]] ] = {6},[ [[Soundtrack]] ] = {7},},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
-{[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[ ]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Legacy%20UI.mpackage]],[ [[desc]] ] = [[Legacy's GUI]],},
+{[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Bashing Package.]],},
+{[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Curing%20Swaps.xml]],[ [[desc]] ] = [[Curing Swap function, downloading it through extras will allow you to keep it between updates of Legacy.]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
 {[ [[link]] ] = [[https://github.com/MegophrysNasuta/SOUNDTRACK/raw/main/SOUNDTRACK.mpackage]],[ [[desc]] ] = [[Nasuta's Ambient Soundtracks, changes based on environments]],},
 }
