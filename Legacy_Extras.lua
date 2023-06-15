2589ff246b81ead7a1c2248a4510ea802fdf48cf
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-06-15 18:18:24 -0500

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,6 +1,6 @@
 return {{[ [[Trades]] ] = {2},[ [[AK Opponent Tracking]] ] = {3},[ [[Legacy GUI]] ] = {4},[ [[Basher]] ] = {5},[ [[Curing Swaps]] ] = {6},[ [[Limb Tracker]] ] = {7},[ [[Soundtrack]] ] = {8},},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
-{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/AK%208.2.zip]],[ [[desc]] ] = [[Austere's Affliction Tracking.]],},
+{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/AK.xml]],[ [[desc]] ] = [[Austere's Affliction Tracking.]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Legacy%20UI.mpackage]],[ [[desc]] ] = [[Legacy's GUI]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Bashing Package.]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Curing%20Swaps.xml]],[ [[desc]] ] = [[Curing Swap function, downloading it through extras will allow you to keep it between updates of Legacy.]],},
