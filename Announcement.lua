2afad0ebeb79c158ee396a78288490773b5ddf39
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-11-03 19:11:04 -0500

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,3 +1,5 @@
 return {{[1] = [[
-Hello World!]],},
+        There will be an update coming soon for the main Legacy system
+        Please make sure to save anything you have done seperately from
+        the main package!!!]],},
 }
