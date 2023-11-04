155e54d8903c71c167f6b16f4e413b731e32f138
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-11-04 18:02:51 -0500

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,5 +1,8 @@
 return {{[1] = [[
-        There will be an update coming soon for the main Legacy system
-        Please make sure to save anything you have done seperately from
-        the main package!!!]],},
+        Thank you for for updating to Legacy V1.5! Most of the changes
+        are things that effected efficeny in the background as well as
+        fixing some issues with tracking classes properly.
+        We hope you all are enjoying the System, if you feel like anything 
+        is out of order, please use the FEEDBACK command and we will get it
+        fixed as soon as we can.!!]],},
 }
