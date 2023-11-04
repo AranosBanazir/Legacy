f79998a9d3a036cdce40d6a21002a1705a81f6cf
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-11-03 19:00:03 -0500

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,3 +1,4 @@
-return {{[1] = [[The Bashing Package has an update coming this saturday, those that use 
-    the package should be on the lookout!]],},
+return {{[1] = [[
+        The Bashing Package has an update coming this saturday, those that use 
+        the package should be on the lookout!]],},
 }
