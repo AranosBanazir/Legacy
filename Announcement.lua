a76a4728161eb010f03a63ae4d1cfadda2ac04ec
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2023-11-15 00:00:24 -0600

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,8 +1,2 @@
-return {{[1] = [[
-        Thank you for for updating to Legacy V1.6! There is a known issue
-        with defense sets not swapping on class change, there is a fix
-        posted on the Legacy Discord, as well as the in game Clan.
-        If you need any help with it feel free to reach out.
-        
-        ]],},
+return {{[1] = [[]],},
 }
