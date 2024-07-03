fed61a0ceacf45d79f7e276631c59fd4b5b7866f
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-07-03 18:57:51 -0500

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -3,7 +3,7 @@ return {{[ [[AK Opponent Tracking]] ] = {2},[ [[Basher]] ] = {3},[ [[Legacy GUI]
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Bashing Package.]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Legacy%20UI.mpackage]],[ [[desc]] ] = [[Legacy's GUI]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
-{[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/blob/main/Shrines%20V3.0.mpackage]],[ [[desc]] ] = [[Package to track, display, and autowalk to shrines.]],},
+{[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Shrines%20V3.0.mpackage]],[ [[desc]] ] = [[Package to track, display, and autowalk to shrines.]],},
 {[ [[link]] ] = [[https://ire-mudlet-mapping.github.io/ire-mapping-script/downloads/mudlet-mapper.xml]],[ [[desc]] ] = [[Mudlet Mapper for Achaea.]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Curing%20Swaps.xml]],[ [[desc]] ] = [[Curing Swap function, downloading it through extras will allow you to keep it between updates of Legacy.]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
