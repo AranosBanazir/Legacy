2c6ac2372f2d469493b002a420d20c26de71b44a
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2025-02-24 12:58:52 -0600

    Update Legacy_Version_Info.lua

--- Legacy_Version_Info.lua
+++ Legacy_Version_Info.lua
@@ -1,2 +1,2 @@
-return {{[ [[msg]] ] = [[Click here to update to <gold>Legacy V]],[ [[Version]] ] = [[1.6]],[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Legacy_Core.mpackage]],},
+return {{[ [[msg]] ] = [[Click here to update to <gold>Legacy V]],[ [[Version]] ] = [[2.0]],[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Legacy_Core.mpackage]],},
 }
