9870c9995c0aebf446362d222ee0b8766c9d6299
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2025-02-25 05:05:34 -0600

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,5 +1,5 @@
 return {{[1] = [[Appologies for jumpscare, as this is a rarely used form of communication!
-The Legacy update to version 2.1 also comes with an update to the Basher and the GUI.
+The Legacy update to version 2.1 also comes with an update to the Basher v2.1 and the GUI v2.0.
 Both of these are available through the extras feature. This announce will remain 
 active for a week, if you wish to stop seeing it you can type 'legacy' and check the 
 'See Legacy System Messages on Login'. Thanks to everyone for their patience and help!]],},
