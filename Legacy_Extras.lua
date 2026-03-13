d6b96f9de57fa1dac12d6c4db94b0a763bb0a212
Michael Guerin <7375201+Sebguer@users.noreply.github.com>
2026-03-13 12:13:55 -0700

    Update Legacy_Extras.lua
    Update Romaen to 1.3

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -6,6 +6,6 @@ return {{[ [[AK Opponent Tracking]] ] = {2},[ [[Basher]] ] = {3},[ [[Legacy GUI]
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/refs/heads/main/SLC.mpackage]],[ [[desc]] ] = [[Self Limb Counting package]],},
 {[ [[link]] ] = [[https://ire-mudlet-mapping.github.io/ire-mapping-script/downloads/mudlet-mapper.xml]],[ [[desc]] ] = [[Mudlet Mapper for Achaea.]],},
 {[ [[link]] ] = [[https://github.com/Legacy-System/Legacy/raw/main/Curing%20Swaps.xml]],[ [[desc]] ] = [[Curing Swap function, downloading it through extras will allow you to keep it between updates of Legacy.]],},
-{[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
+{[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.3/limb.1.3.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
 {[ [[link]] ] = [[https://github.com/MegophrysNasuta/SOUNDTRACK/raw/main/SOUNDTRACK.mpackage]],[ [[desc]] ] = [[Nasuta's Ambient Soundtracks, changes based on environments]],},
 }
